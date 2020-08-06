3777433eae55c81712fd0e753871e4ee9c2488ea
きむらあやか <ha.iy.r8ao@gmail.comgit config --global user.name chorin11>
2020-08-06 15:21:14 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   attachment :profile_image
 
   validates :name, presence: true, length: {in: 2..20}
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:index, :create, :update, :show, :edit]
+
+  resources :relationships, only: [:create, :destroy]
 end
